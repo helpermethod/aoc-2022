f7b19fdeda636e0aea9f566c2dcf7208bb337839
helpermethod <oliver@sdkman.io>
2022-12-03 09:27:02 +0100

    Refactor

--- day-3/part-2.main.kts
+++ day-3/part-2.main.kts
@@ -5,13 +5,14 @@ import java.io.File
 val translate =
     (('A'..'Z').zip(27..52) + ('a'..'z').zip(1..27)).toMap()
 
+fun backpacks(group: List<String>) =
+    group.map { it.toSet() }
+
 File("input.txt")
     .useLines { lines ->
         lines
             .chunked(3)
-            .map { group ->
-                group.map { it.toSet() }
-            }
+            .map(::backpacks)
             .map { backpacks ->
                 backpacks.reduce { left, right -> left.intersect(right) }.first()
             }
