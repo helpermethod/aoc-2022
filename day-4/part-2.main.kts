c8f05dd65ab74448624111b728ede0e7dfa9bb96
Oliver Weiler <oliver@sdkman.io>
2022-12-04 22:27:01 +0100

    Rename toIntSet to toRangeSet

--- day-4/part-2.main.kts
+++ day-4/part-2.main.kts
@@ -2,13 +2,13 @@
 
 import java.io.File
 
-fun String.toIntSet() =
+fun String.toRangeSet() =
     split("-").let { (start, end) -> (start.toInt()..end.toInt()).toSet() }
 
 File("input.txt")
     .useLines { lines ->
         lines
             .map { it.split(",") }
-            .map { (first, second) -> first.toIntSet() to second.toIntSet() }
+            .map { (first, second) -> first.toRangeSet() to second.toRangeSet() }
             .count { (first, second) -> first.intersect(second).isNotEmpty() }
     }
